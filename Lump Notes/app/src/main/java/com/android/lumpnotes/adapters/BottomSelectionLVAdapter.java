cbdb98b321cf79c1cd5b4a82aa11f06d0bdd7927
vibin <vibin2joby@gmail.com>
2020-02-07 11:16:07 -0500

    new resource directory changes for images

---
+++ Lump Notes/app/src/main/java/com/android/lumpnotes/adapters/BottomSelectionLVAdapter.java
@@ -0,0 +1,49 @@
+package com.android.lumpnotes.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.lumpnotes.R;
+
+public class BottomSelectionLVAdapter extends BaseAdapter {
+    String[] selectionArr;
+    Context context;
+    public BottomSelectionLVAdapter(String[] selectionArr,Context context) {
+        this.selectionArr = selectionArr;
+        this.context = context;
+    }
+    @Override
+    public int getCount() {
+        return selectionArr.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return selectionArr[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.add_category_note,null);
+        TextView selectionTxt = convertView.findViewById(R.id.selectionTxt);
+        selectionTxt.setText(selectionArr[position]);
+
+        ImageView selectionImg= convertView.findViewById(R.id.selectionIC);
+        if(position == 0) {
+            selectionImg.setImageResource(R.drawable.new_category);
+        } else {
+            selectionImg.setImageResource(R.drawable.new_note);
+        }
+        return convertView;
+    }
+}
--- Lump Notes/app/src/main/java/com/android/lumpnotes/fragment/BottomSheetFragment.java
+++ Lump Notes/app/src/main/java/com/android/lumpnotes/fragment/BottomSheetFragment.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.android.lumpnotes.R;
+import com.android.lumpnotes.adapters.BottomSelectionLVAdapter;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class BottomSheetFragment extends BottomSheetDialogFragment {
@@ -18,6 +20,11 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.bottom_sheet_fragment, container, false);
+        ListView selectionListView = view.findViewById(R.id.selectionList);
+        String[] selectionList = {"New Category","New Note"};
+        BottomSelectionLVAdapter adapter = new BottomSelectionLVAdapter(selectionList,this.getContext());
+
+        selectionListView.setAdapter(adapter);
         view.findViewById(R.id.closeBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
